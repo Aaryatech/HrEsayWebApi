01a7029f94f0ea21beb809b0d7bf0d16a5892e68
lenovo <lenovo@lenovo-Lenovo-ideapad-320-15IKB>
2019-12-03 12:53:54 +0530

    /Akshay

--- src/main/java/com/ats/hrmgt/controller/AccessRoleRestController.java
+++ src/main/java/com/ats/hrmgt/controller/AccessRoleRestController.java
@@ -7,8 +7,18 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.ats.hrmgt.model.AccessRightModule;
+import com.ats.hrmgt.model.AccessRightSubModule;
+import com.ats.hrmgt.model.EmpType;
+import com.ats.hrmgt.model.Info;
+import com.ats.hrmgt.repository.AccessRightModuleRepository;
+import com.ats.hrmgt.repository.AccessRightSubModuleRepository;
+import com.ats.hrmgt.repository.EmpTypeRepository;
+ 
  
 
 @RestController
@@ -16,13 +26,132 @@ public class AccessRoleRestController {
  
 	 
 	
-	@RequestMapping(value = { "/initializeWebService" }, method = RequestMethod.GET)
-	public @ResponseBody String initializeWebService() {
+	@Autowired
+	AccessRightModuleRepository accessRightModuleRepository;
+	
+	@Autowired
+	AccessRightSubModuleRepository accessRightSubModuleRepository;
+	
+	@Autowired
+	EmpTypeRepository empTypeRepository;
+	
+	@RequestMapping(value = { "/getModuleAndSubModuleList" }, method = RequestMethod.GET)
+	public @ResponseBody List<AccessRightModule> getModuleAndSubModuleList() {
 
 		 
-		 
+		List<AccessRightModule> accessRightModuleList = new ArrayList<>();
+		try {
+			 
+			 accessRightModuleList = accessRightModuleRepository.getModule();
 
-		return "Success";
+			for (int i = 0; i < accessRightModuleList.size(); i++) {
+
+				
+				List<AccessRightSubModule> accessRightSubModuleList = accessRightSubModuleRepository
+						.getSubModule(accessRightModuleList.get(i).getModuleId());
+				accessRightModuleList.get(i).setAccessRightSubModuleList(accessRightSubModuleList);
+
+			}
+			System.out.println(accessRightModuleList);
+		} catch (Exception e) {
+ 
+			e.printStackTrace();
+		}
+
+		return accessRightModuleList;
+
+	}
+	
+	@RequestMapping(value = { "/saveEmpType" }, method = RequestMethod.POST)
+	public @ResponseBody EmpType saveEmpType(@RequestBody EmpType empType) {
+
+		EmpType save = new EmpType();
+		try {
+
+			save = empTypeRepository.saveAndFlush(empType);
+
+			if (save == null) {
+
+				save = new EmpType();
+				save.setError(true);
+
+			} else {
+				save.setError(false);
+			}
+
+		} catch (Exception e) {
+
+			e.printStackTrace();
+			save.setError(true);
+		}
+
+		return save;
+
+	}
+
+	@RequestMapping(value = { "/getEmpTypeList" }, method = RequestMethod.POST)
+	public @ResponseBody List<EmpType> getEmpTypeList(@RequestParam("compId") int compId) {
+
+		List<EmpType> list = new ArrayList<EmpType>();
+		try {
+
+			if (compId != 0) {
+				list = empTypeRepository.findByDelStatusAndCompanyId(1, compId);
+			} else {
+				list = empTypeRepository.findByDelStatus(1);
+			}
+
+		} catch (Exception e) {
+
+			e.printStackTrace();
+		}
+
+		return list;
+
+	}
+
+	@RequestMapping(value = { "/deleteEmpType" }, method = RequestMethod.POST)
+	public @ResponseBody Info deleteEmpType(@RequestParam("empTypeId") int empTypeId) {
+
+		Info info = new Info();
+
+		try {
+
+			int delete = empTypeRepository.deleteEmpType(empTypeId);
+
+			if (delete > 0) {
+				info.setError(false);
+				info.setMsg("deleted");
+			} else {
+				info.setError(true);
+				info.setMsg("failed");
+			}
+
+		} catch (Exception e) {
+
+			e.printStackTrace();
+			info.setError(true);
+			info.setMsg("failed");
+		}
+
+		return info;
+
+	}
+	
+	@RequestMapping(value = { "/getEmpTypeById" }, method = RequestMethod.POST)
+	public @ResponseBody EmpType getEmpTypeById(@RequestParam("empTypeId") int empTypeId) {
+
+		EmpType location = new EmpType();
+		try {
+
+			location = empTypeRepository.findByEmpTypeIdAndDelStatus(empTypeId, 1);
+
+		} catch (Exception e) {
+
+			e.printStackTrace();
+		}
+
+		return location;
 
 	}
 	
