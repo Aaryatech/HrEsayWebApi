0e1d2dae0c1fb4a0cc2afe76a6796a2e0db9533c
lenovo <lenovo@lenovo-Lenovo-ideapad-320-15IKB>
2019-12-13 12:18:55 +0530

    /Akshay

--- src/main/java/com/ats/hrmgt/repository/InfoForUploadAttendanceRepository.java
+++ src/main/java/com/ats/hrmgt/repository/InfoForUploadAttendanceRepository.java
@@ -8,7 +8,7 @@ import com.ats.hrmgt.model.InfoForUploadAttendance;
 
 public interface InfoForUploadAttendanceRepository extends JpaRepository<InfoForUploadAttendance, Integer>{
 
-	@Query(value="select COALESCE(( select count(*) from m_employees ),0)  as total_emp, DATEDIFF(:toDate, :fromDate) as date_diff, "
+	@Query(value="select COALESCE(( select count(*) from m_employees,tbl_emp_salary_info where m_employees.emp_id=tbl_emp_salary_info.emp_id),0)  as total_emp, DATEDIFF(:toDate, :fromDate) as date_diff, "
 			+ "COALESCE(( select count(*) from tbl_attt_daily_daily where att_date between :fromDate and :toDate ),0)  as updated_by_step1, "
 			+ "COALESCE(( select count(*) from tbl_attt_daily_daily where att_date between :fromDate and :toDate and by_file_updated=1 ),0) "
 			+ " as updated_by_file",nativeQuery=true)
