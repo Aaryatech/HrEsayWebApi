fa2d89a9992a70743ecda29f8b7ec8bd9a488dee
maddy <maddy@maddy-Inspiron-3541>
2020-02-20 18:44:20 +0530

    //maddy

--- src/main/java/com/ats/hrmgt/controller/ReportsApiController.java
+++ src/main/java/com/ats/hrmgt/controller/ReportsApiController.java
@@ -11,13 +11,17 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ats.hrmgt.model.DailyAttendance;
+import com.ats.hrmgt.model.report.LoanStatementDetailsReport;
 import com.ats.hrmgt.model.report.PendingLoanReport;
+import com.ats.hrmgt.repo.report.LoanStatementRepo;
 import com.ats.hrmgt.repo.report.PendingLoanRepo;
 
 @RestController
 public class ReportsApiController {
 
 	@Autowired PendingLoanRepo pendLoanRepo;
+	
+	@Autowired LoanStatementRepo loanStatRepo;
 	
 	@RequestMapping(value = { "/getEmpPendingLoanReport" }, method = RequestMethod.POST)
 	public @ResponseBody List<PendingLoanReport> getEmpPendingLoanReport(@RequestParam("companyId") int companyId,
@@ -28,6 +32,25 @@ public class ReportsApiController {
 		try {
 
 			list = pendLoanRepo.getEmpPendingLoanDetails(companyId, fromDate, toDate);
+
+		} catch (Exception e) {
+
+			e.printStackTrace();
+		}
+
+		return list;
+
+	}
+	
+	@RequestMapping(value = { "/getLoanStatemnetReport" }, method = RequestMethod.POST)
+	public @ResponseBody List<LoanStatementDetailsReport> getLoanStatemnetReport(@RequestParam("companyId") int companyId,
+			@RequestParam("fromDate") String fromDate, @RequestParam("toDate") String toDate) {
+
+		List<LoanStatementDetailsReport> list = new ArrayList<LoanStatementDetailsReport>();
+
+		try {
+
+			list = loanStatRepo.getEmpLoanStateDetails(companyId, fromDate, toDate);
 
 		} catch (Exception e) {
 
