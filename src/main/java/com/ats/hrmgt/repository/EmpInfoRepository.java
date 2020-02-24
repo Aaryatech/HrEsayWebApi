c0da65ff145e54389b5dbc05b062ff393ceee950
lenovo <lenovo@lenovo-Lenovo-ideapad-320-15IKB>
2020-02-24 13:22:37 +0530

    /Akshay

--- src/main/java/com/ats/hrmgt/repository/EmpInfoRepository.java
+++ src/main/java/com/ats/hrmgt/repository/EmpInfoRepository.java
@@ -20,7 +20,7 @@ public interface EmpInfoRepository extends JpaRepository<EmpInfo, Integer> {
 	List<EmpInfo> getEmpListAll(@Param("fromDate") String fromDate);
 	
 	@Query(value = "SELECT e.*,emp_sal.cmp_joining_date, emp_sal.sal_basis, emp_sal.salary_type_id  FROM m_employees e, tbl_emp_salary_info emp_sal "
-			+ "where e.emp_id=emp_sal.emp_id and e.del_status=1 )", nativeQuery = true)
+			+ "where e.emp_id=emp_sal.emp_id and e.del_status=1", nativeQuery = true)
 	List<EmpInfo> getEmpListAll();
 
 }
